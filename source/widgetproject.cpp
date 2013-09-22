bdcb7012086ac8861a28a37c833ed789b665da6e
Quentin Bramas <quentin.bramas@gmail.com>
2013-09-22 16:26:50 +0200

    begin the organization of files into project

---
+++ source/widgetproject.cpp
@@ -0,0 +1,7 @@
+#include "widgetproject.h"
+
+WidgetProject::WidgetProject(QWidget *parent) :
+    QWidget(parent)
+{
+    this->setStyleSheet("background-color:#ff0000");
+}
---
+++ source/widgetproject.h
@@ -0,0 +1,18 @@
+#ifndef WIDGETPROJECT_H
+#define WIDGETPROJECT_H
+
+#include <QWidget>
+
+class WidgetProject : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit WidgetProject(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // WIDGETPROJECT_H
