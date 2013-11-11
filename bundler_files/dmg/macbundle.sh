4f4b84b3e2e15b43bb0789a9a5c6a3ad6a6bdc24
Quentin Bramas <quentin.bramas@gmail.com>
2013-11-11 21:15:26 +0100

    macbundle

--- bundler_files/dmg/macbundle.sh
+++ bundler_files/dmg/macbundle.sh
@@ -8,7 +8,7 @@ rm -r /tmp/texiteasy/*
 cp /ApplicationsAlias /tmp/texiteasy/Applications
 cp -r ../../../build/Qt_5_1_1_clang_64bit-Release/texiteasy.app /tmp/texiteasy/texiteasy.app
 
-hdiutil create /tmp/texiteasy-0.6.0.dmg -ov -volname "texiteasy-0.6.0" -fs HFS+ -srcfolder "/tmp/texiteasy/"
-hdiutil convert /tmp/texiteasy-0.6.0.dmg -format UDZO -o ~/Documents/texiteasy-0.6.0_mac.dmg
+hdiutil create /tmp/texiteasy-$1.dmg -ov -volname "texiteasy-0.6.0" -fs HFS+ -srcfolder "/tmp/texiteasy/"
+hdiutil convert /tmp/texiteasy-$1.dmg -format UDZO -o ~/Documents/texiteasy-$1_mac.dmg
 
 sudo rm -rf ../../../build/Qt_5_1_1_clang_64bit-Release/texiteasy.app
