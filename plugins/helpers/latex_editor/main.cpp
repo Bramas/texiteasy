6be45b0eb3dc4de8929625dd5326abb1256fa3ba
Quentin Bramas <quentin.bramas@gmail.com>
2014-05-21 13:03:01 +0200

    test on mac

--- plugins/helpers/latex_editor/main.cpp
+++ plugins/helpers/latex_editor/main.cpp
@@ -5,7 +5,11 @@ int main(int argc, char *argv[])
 {
     QGuiApplication a(argc, argv);
 
+#ifdef MAC_OS_X_VERSION_10_0
+    QQuickView viewer(QUrl::fromLocalFile("/Users/quentinbramas/Projects/texiteasy/texiteasy-repository/plugins/helpers/latex_editor/qml/main.qml"));
+#else
     QQuickView viewer(QUrl::fromLocalFile("D:/Projects/texiteasy/sourceRepository/plugins/helpers/latex_editor/qml/main.qml"));
+#endif
     viewer.show();
     return a.exec();
 }
